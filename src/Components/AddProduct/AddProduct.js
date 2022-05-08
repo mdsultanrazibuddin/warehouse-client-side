c2905b57d0b6d505bfdc086ad6fa623b96707401
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-09 01:09:24 +0600

    add product created

--- src/Components/AddProduct/AddProduct.js
+++ src/Components/AddProduct/AddProduct.js
@@ -23,8 +23,10 @@ const AddProduct = () => {
             <h1 className='text-center mt-3 mb-5'>Add Product Item</h1>
              <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
       <input className='mb-2'  placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+      <input className='mb-2'  placeholder='Supplier Name' {...register("name", { required: true, maxLength: 20 })} />
       <textarea className='mb-2' placeholder='Description' {...register("description")} />
       <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+      <input className='mb-2' placeholder='Quantity' type="number" {...register("Quantity")} />
       <input className='mb-2' placeholder='Photo URL' type="text" {...register("img")} />
       <input className='mb-2'  type="submit" value="Add Product" />
     </form>
