16fd6ad6ba0c936573a1ef7bd8432c4f47226ebd
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-09 01:49:25 +0600

    added

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -6,7 +6,7 @@ import {  Link } from 'react-router-dom';
 import './Products.css'
 
 const Products = ({product}) => {
-    const {img, name, price} = product;
+    const {img, description, quantity, supplier, name, price} = product;
     
     return (
         <div className="col-lg-4 mb-5 mt-5  service-container">
@@ -15,12 +15,15 @@ const Products = ({product}) => {
             <div className="  card-body">
                     <img className="product-img" src={img} alt="img" />
                     <h4 className="card-title text-center my-4">{name}</h4>
+                    <p className="card-title text-center my-4">{description}</p>
+                    <h5 className="card-title text-center my-4">Supplier Name : {supplier}</h5>
+                    <h4 className="card-title text-center my-4">Quantity : {quantity}</h4>
                     
                     <h4 className="card-price text-center">Price: {price}</h4>
                 <div className="d-flex justify-content-center     align-items-center">
                         <div className="mt-1">
                             {/* <button onClick={ () => navigateToProductDetail(_id)}   className="btn button">{_id}</button> */}
-                            <Link className='Button-title navbar' to ="/ProductDetails">Order Now</Link>
+                            <Link className='Button-title navbar' to ="/ProductDetails">Update</Link>
                            
                         </div>
                     </div>
