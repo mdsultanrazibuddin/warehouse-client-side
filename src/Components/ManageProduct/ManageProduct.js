1ef5f7a4c38f9d3e71a3a84897007b23f67509ce
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-08 13:19:18 +0600

    manage product create

--- src/Components/ManageProduct/ManageProduct.js
+++ src/Components/ManageProduct/ManageProduct.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useProduct from '../Hooks/useProduct';
 
 const ManageProduct = () => {
+    const [product] = useProduct();
+    const handleDelete = id =>{
+        const proceed = window.confirm('Are you Sure');
+        if(proceed){
+            
+        }
+    }
     return (
-        <div>
-            <h1>hhhhhhhhhhhh</h1>
+        <div className='w-50 mx-auto'>
+           
+
+            {
+                product.map(product => <div key ={product._id}>
+                    <h6>{product.name} <button onClick={() => handleDelete() }>x</button></h6>
+
+
+                </div>)
+            }
         </div>
     );
 };
