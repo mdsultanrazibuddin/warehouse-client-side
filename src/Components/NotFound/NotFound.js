5a6b2d8205b2eb715e7d76acc713b8bad57589f6
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-05-08 23:41:44 +0600

    notfound file created

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
+import image from '../../Images/254-2549834_404-page-not-found-404-not-found-png.png'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Not Found Anything</h1>
+        <div className='notfound'>
+          <img src={image} alt="" />
         </div>
     );
 };
